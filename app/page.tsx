b7c2842d4d694b808ec8c78d23b48eb965fba06e
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-08-16 18:56:18 +0530

    feat: add Testimonials, Blogs, and Projects components to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import { Hero, UpcomingEvent } from "@/components/LandingPage";
+import {
+  Testimonials,
+  Hero,
+  UpcomingEvent,
+  Blogs,
+  Projects,
+} from "@/components/LandingPage";
 
 export default function Page() {
   return (
     <main className="relative min-h-screen">
       <Hero />
       <UpcomingEvent />
+      <Testimonials />
+      <Blogs />
+      <Projects />
     </main>
   );
 }
