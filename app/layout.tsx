a0e5d2a039ae895e07335cefdbf57ff22d958e56
nnk25 <2023cs088@stu.ucsc.cmb.ac.lk>
2025-08-16 22:04:37 +0530

    Reformatted about us page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
 import "./globals.css";
 
 import LenisWrapper from "@/components/LenisWrapper";
@@ -7,6 +7,12 @@ import Header from "@/components/Header";
 import { ElementalEnd } from "@/fonts";
 import Footer from "@/components/Footer";
 import BackgroundWrapper from "@/components/common/BgWrapper";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  weight: ["400", "500"],
+  subsets: ["latin"]
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +37,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${ElementalEnd.variable} relative antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${ElementalEnd.variable} ${poppins.variable} relative antialiased`}
       >
         <LenisWrapper>
           <BackgroundWrapper>
