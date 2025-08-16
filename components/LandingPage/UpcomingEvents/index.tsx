cac2f824c5dd693a45e38d80f7f89eaf6e335806
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-08-16 18:56:11 +0530

    fix: adjust padding in UpcomingEvents Item and section for better layout

--- components/LandingPage/UpcomingEvents/index.tsx
+++ components/LandingPage/UpcomingEvents/index.tsx
@@ -11,7 +11,7 @@ import { upcomingEventsData } from "@/lib/data";
 
 export default function UpcomingEvent() {
   return (
-    <section className="container mx-auto flex flex-col gap-y-8">
+    <section className="section-gap-y container mx-auto flex flex-col gap-y-8">
       <h1 className="section-header">
         <span>Upcoming</span>
         <span className="text-primary"> Events</span>
