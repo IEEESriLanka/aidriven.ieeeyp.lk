71d68ff7f61f523438d47f2da4afcef29a9915bd
nnk25 <2023cs088@stu.ucsc.cmb.ac.lk>
2025-08-20 22:34:50 +0530

    Changed fixed witdths to containers in Form component

--- components/AboutUs/Form.tsx
+++ components/AboutUs/Form.tsx
@@ -1,21 +1,21 @@
 const Form = () => {
     const inputStyle =
-        "p-[15.8px_24.8px_17.4px_24.8px] rounded-[8px] border-1 border-primary justify-center w-full bg-[#191919] text-[14px] text-white focus:outline-none focus:border-2 ";
+        "p-[15.8px_24.8px_17.4px_24.8px] rounded-[8px] border-1 border-primary justify-center container bg-[#191919] text-[14px] text-white focus:outline-none focus:border-2 ";
     const labelStyle = "flex text-[16px] font-[500]";
     const Star = () => <div className="text-[16px] text-primary">*</div>;
 
     return (
-        <div className="flex flex-col py-[40px] w-full md:w-[846px] px-[44px] bg-[#45413DCC] rounded-[4px_24px] gap-[20px]">
+        <div className="flex flex-col py-[40px] container px-[44px] bg-[#45413DCC] rounded-[4px_24px] gap-[20px]">
             <p className="uppercase font-elemental-end text-[20px] text-white">contact us</p>
 
-            <div className="flex flex-col md:flex-row justify-between w-full gap-[20px] md:gap-[100px]">
-                <div className="w-full flex flex-col gap-[20px]">
+            <div className="flex flex-col md:flex-row justify-between container gap-[20px] md:gap-[100px]">
+                <div className="container flex flex-col gap-[20px]">
                     <label className={labelStyle} htmlFor="fname">
                         First Name<Star />
                     </label>
                     <input name="fname" type="text" className={inputStyle} placeholder="First name here" required />
                 </div>
-                <div className="w-full flex flex-col gap-[20px]">
+                <div className="container flex flex-col gap-[20px]">
                     <label className={labelStyle} htmlFor="lname">
                         Last Name<Star />
                     </label>
@@ -23,14 +23,14 @@ const Form = () => {
                 </div>
             </div>
 
-            <div className="w-full flex flex-col gap-[20px]">
+            <div className="container flex flex-col gap-[20px]">
                 <label className={labelStyle} htmlFor="email">
                     Email Address<Star />
                 </label>
                 <input name="email" type="email" className={inputStyle} placeholder="Add email" required />
             </div>
 
-            <div className="w-full flex flex-col gap-[20px]">
+            <div className="container flex flex-col gap-[20px]">
                 <label className={labelStyle} htmlFor="comments">
                     Comments/Questions
                 </label>
