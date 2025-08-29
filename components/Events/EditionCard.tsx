ce1fe4410b36fc31dd2e383d0407af4f87c104a8
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-08-29 23:38:29 +0530

    feat: Update EditionCard component to use title prop and enhance styling

--- components/Events/EditionCard.tsx
+++ components/Events/EditionCard.tsx
@@ -2,20 +2,18 @@ import type { eventsEditionDataProps } from "@/lib/type";
 import { CalendarDays, Ticket, Users } from "lucide-react";
 import Image from "next/image";
 
-const EditionCard = ({ year, desc, data, imgs }: eventsEditionDataProps) => {
+const EditionCard = ({ title, desc, data, imgs }: eventsEditionDataProps) => {
   return (
-    <div className="edition-card container flex flex-col items-center gap-10 p-8">
+    <div className="edition-card container flex flex-col items-center gap-10 py-4 lg:p-8">
       <div className="container flex flex-col items-center justify-center gap-10 p-[10px] text-white md:p-[70px]">
-        <h3 className="text-primary text-4xl font-bold">
-          Community Talk Sries <span>{year}</span>
-        </h3>
-        <p>{desc}</p>
+        <h3 className="text-primary text-4xl font-bold">{title}</h3>
+        <p className="lg:text-xl">{desc}</p>
       </div>
       <div className="flex w-fit flex-col justify-between gap-x-16 gap-y-2 lg:flex-row lg:items-center">
         <div className="flex items-center gap-x-2">
           <Users className="text-primary" />
           <div className="flex flex-col">
-            <span className="font-elemental-end text-xl font-semibold text-white">
+            <span className="font-elemental-end text-2xl font-semibold text-white">
               {data.participateCount}+
             </span>
             <span className="text-white/60 uppercase">participants</span>
@@ -23,13 +21,13 @@ const EditionCard = ({ year, desc, data, imgs }: eventsEditionDataProps) => {
         </div>
         <div className="flex items-center gap-x-2">
           <Ticket className="text-primary" />
-          <span className="font-elemental-end text-xl font-semibold text-white">
+          <span className="font-elemental-end text-2xl font-semibold text-white">
             {data.edition}
           </span>
         </div>
         <div className="flex items-center gap-x-2">
           <CalendarDays className="text-primary" />
-          <span className="font-elemental-end text-xl font-semibold text-white uppercase">
+          <span className="font-elemental-end text-2xl font-semibold text-white uppercase">
             {data.date}
           </span>
         </div>
