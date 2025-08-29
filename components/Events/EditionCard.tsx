57d791aad9dad17478fb874f3da642d2f5e6c49d
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-08-29 22:48:03 +0530

    fix: Refactor EditionCard component: update props structure and enhance layout

--- components/Events/EditionCard.tsx
+++ components/Events/EditionCard.tsx
@@ -1,31 +1,52 @@
-import EventCard from './EventCard'
-import { ContactCard } from '../AboutUs/Cards'
-import { ContactDetailProps } from '@/lib/type'
-import Image, { StaticImageData } from 'next/image'
+import type { eventsEditionDataProps } from "@/lib/type";
+import { CalendarDays, Ticket, Users } from "lucide-react";
+import Image from "next/image";
 
-
-
-const EditionCard = ({year, desc, stats, imgs}: {year: string, desc: string, stats:ContactDetailProps[], imgs: StaticImageData[]}) => {
+const EditionCard = ({ year, desc, data, imgs }: eventsEditionDataProps) => {
   return (
-    <EventCard className='flex flex-col container gap-10'>
-    <div className='container flex flex-col justify-center gap-10 items-center text-white p-[10px] md:p-[70px]'>
-      <p className='text-primary text-[32px]'>Community Talk Sries <span>{year}</span></p>
-      <p>{desc}</p>
-    </div>
-    <div className='flex flex-col md:flex-row  gap-8 container justify-center'>
-        {stats.map((stat,i) => (
-            <ContactCard key={i} contactDetail={stat}/>
+    <div className="edition-card container flex flex-col items-center gap-10 p-8">
+      <div className="container flex flex-col items-center justify-center gap-10 p-[10px] text-white md:p-[70px]">
+        <h3 className="text-primary text-4xl font-bold">
+          Community Talk Sries <span>{year}</span>
+        </h3>
+        <p>{desc}</p>
+      </div>
+      <div className="flex w-fit flex-col justify-between gap-x-16 gap-y-2 lg:flex-row lg:items-center">
+        <div className="flex items-center gap-x-2">
+          <Users className="text-primary" />
+          <div className="flex flex-col">
+            <span className="font-elemental-end text-xl font-semibold text-white">
+              {data.participateCount}+
+            </span>
+            <span className="text-white/60 uppercase">participants</span>
+          </div>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <Ticket className="text-primary" />
+          <span className="font-elemental-end text-xl font-semibold text-white">
+            {data.edition}
+          </span>
+        </div>
+        <div className="flex items-center gap-x-2">
+          <CalendarDays className="text-primary" />
+          <span className="font-elemental-end text-xl font-semibold text-white uppercase">
+            {data.date}
+          </span>
+        </div>
+      </div>
+
+      <div className="container flex flex-col items-center justify-center md:flex-row">
+        {imgs.map((img, i) => (
+          <Image
+            key={i}
+            className="container h-[250px] object-contain"
+            src={img}
+            alt=""
+          />
         ))}
+      </div>
     </div>
+  );
+};
 
-    <div className="flex flex-col md:flex-row container justify-center items-center">
-        {imgs.map((img,i) => (
-            <Image key={i} className='container h-[250px] object-contain' src={img} alt=""/>
-        ))}
-    </div>
-
-  </EventCard>
-  )
-}
-
-export default EditionCard
+export default EditionCard;
