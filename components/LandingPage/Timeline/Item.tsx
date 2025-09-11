28dad5735db479094d77fab6c9002dbe60ad2ce2
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-09-11 20:24:18 +0530

    feat: enhance Timeline component with active state management and improved styling

--- components/LandingPage/Timeline/Item.tsx
+++ components/LandingPage/Timeline/Item.tsx
@@ -1,15 +1,29 @@
 import type { TimelineItemProps } from "@/lib/type";
 import { CalendarDays, Clock4 } from "lucide-react";
+import { useEffect } from "react";
 
-export default function Item(props: TimelineItemProps) {
+type Props = TimelineItemProps & {
+  isActive?: boolean;
+};
+
+export default function Item({ isActive, ...props }: Props) {
+  useEffect(() => {
+    console.log(isActive);
+  }, [isActive]);
   return (
     <article
-      className="_min-h-[380px] flex max-w-[480px] flex-col justify-between gap-y-4 bg-[rgba(69,65,61,0.8)] px-4 py-8"
+      className={`_max-w-[480px] flex min-h-[300px] flex-col justify-between gap-y-4 px-4 py-8 text-white transition-colors duration-300 ${
+        isActive ? "bg-primary!" : "timeline-card"
+      }`}
       style={{
         borderRadius: "20px 20px 0 20px",
       }}
     >
-      <h3 className="text-2xl font-medium text-white">{props.title}</h3>
+      <h3
+        className={`text-2xl font-medium lg:text-4xl ${isActive ? "text-white" : "text-primary"} transition-colors duration-300`}
+      >
+        {props.title}
+      </h3>
       <div className="flex flex-col items-center gap-y-4">
         <h3 className="text-center">{props.caption}</h3>
         <div className="flex flex-col items-center gap-y-1">
@@ -19,11 +33,13 @@ export default function Item(props: TimelineItemProps) {
       </div>
       <div className="flex w-full flex-col justify-center gap-4 lg:flex-row lg:items-center">
         <div className="flex items-center gap-x-1">
-          <CalendarDays className="text-primary" />
+          <CalendarDays
+            className={` ${isActive ? "text-white" : "text-primary"}`}
+          />
           <span>{props.data.date}</span>
         </div>
         <div className="flex items-center gap-x-1">
-          <Clock4 className="text-primary" />
+          <Clock4 className={` ${isActive ? "text-white" : "text-primary"}`} />
           <span>{props.data.time}</span>
         </div>
         <span>{props.data.venue}</span>
