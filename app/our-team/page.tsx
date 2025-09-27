5ae0345e4ce8e8b3d306d5a1dc023e41c0e25e2e
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-09-27 10:01:47 +0530

    feat: implement team member display using MemberCard component

--- app/our-team/page.tsx
+++ app/our-team/page.tsx
@@ -1,9 +1,17 @@
+import { MemberCard } from "@/components/AboutUs/Cards";
+import { TeamDetails } from "@/lib/data";
+
 export default function Page() {
   return (
     <main className="container mx-auto my-[100px] flex min-h-screen flex-col gap-[40px]">
       <h3 className="section-header">
         our <span className="text-primary">team</span>
       </h3>
+      <div className="flex items-center gap-x-6 lg:gap-x-16">
+        {TeamDetails["main"].members.map((team) => (
+          <MemberCard key={team.name} member={team} />
+        ))}
+      </div>
     </main>
   );
 }
