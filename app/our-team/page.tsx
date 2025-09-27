81ffbbde204e323a9d815567d03cc896d8eeea35
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-09-27 09:51:55 +0530

    feat: create initial Page component for team section

---
+++ app/our-team/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <main className="container mx-auto my-[100px] flex min-h-screen flex-col gap-[40px]">
+      <h3 className="section-header">
+        our <span className="text-primary">team</span>
+      </h3>
+    </main>
+  );
+}
