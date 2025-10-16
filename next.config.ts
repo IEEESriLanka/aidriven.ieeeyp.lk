b786f207ba26e406a3657895a51f17a21271cac4
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-10-16 16:08:36 +0530

    fix: remove assetPrefix and basePath from next.config.ts for environment flexibility

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,9 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
 
-  assetPrefix: isProd ? '/aidriven.ieeeyp.lk/' : '',
-  basePath: isProd ? '/aidriven.ieeeyp.lk' : '',
   output: "export",
 };
 
