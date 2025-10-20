79afa02fef9b0dfd6eb2e71ee4c03ac5dbe7b50f
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-10-20 20:21:18 +0530

    fix: correct class name in main container for team page layout

--- app/our-team/page.tsx
+++ app/our-team/page.tsx
@@ -3,7 +3,7 @@ import { TeamDetails } from "@/lib/data";
 
 export default function Page() {
   return (
-    <main className="container mx-auto py-[100px] flex min-h-screen flex-col gap-[40px]">
+    <main className="container mx-auto py-[100px] flex min-h-screen flex-col gap-[40px] min-scre">
       <h3 className="section-header">
         our <span className="text-primary">team</span>
       </h3>
