b6dbcbe4ebc44e7a51c16b26002d949419b4993e
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-11-16 11:26:21 +0530

    fix: comment out repoName and related configurations for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
-const repoName = "/aidriven.ieeeyp.lk";
+// const repoName = "/aidriven.ieeeyp.lk";
 
 const nextConfig = {
   images: {
     unoptimized: true,
   },
   // Ensure static assets resolve correctly when hosted at /<repo-name>/ on GitHub Pages.
-  basePath: process.env.DEPLOY_ENV === 'GH_PAGES' ? repoName : '',
-  assetPrefix: process.env.DEPLOY_ENV === 'GH_PAGES' ? `${repoName}/` : '',
-  trailingSlash: true,
+  // basePath: process.env.DEPLOY_ENV === 'GH_PAGES' ? repoName : '',
+  // assetPrefix: process.env.DEPLOY_ENV === 'GH_PAGES' ? `${repoName}/` : '',
+  // trailingSlash: true,
   output: "export",
   distDir: "dist"
 };
