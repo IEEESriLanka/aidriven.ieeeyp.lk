6dbe895d9c6a5eab29672eb7b1b242dd81f12006
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-11-28 17:22:55 +0530

    feat: update site ui components

--- components/Events/EditionCard.tsx
+++ components/Events/EditionCard.tsx
@@ -7,7 +7,17 @@ const EditionCard = ({ title, desc, data, imgs }: eventsEditionDataProps) => {
     <div className="edition-card container flex flex-col items-center gap-10 py-4 lg:p-8">
       <div className="container flex flex-col items-center justify-center gap-10 p-[10px] text-white md:p-[70px]">
         <h3 className="text-primary text-4xl font-bold">{title}</h3>
-        <p className="lg:text-xl">{desc}</p>
+        {Array.isArray(desc) ? (
+          <div className="space-y-4">
+            {desc.map((paragraph, index) => (
+              <p key={index} className="text-center lg:text-xl">
+                {paragraph}
+              </p>
+            ))}
+          </div>
+        ) : (
+          <p className="lg:text-xl">{desc}</p>
+        )}
       </div>
       <div className="flex w-fit flex-col justify-between gap-x-16 gap-y-2 lg:flex-row lg:items-center">
         <div className="flex items-center gap-x-2">
@@ -34,7 +44,7 @@ const EditionCard = ({ title, desc, data, imgs }: eventsEditionDataProps) => {
       </div>
 
       <div className="container flex flex-col items-center justify-center md:flex-row">
-        {imgs.map((img, i) => (
+        {imgs?.map((img, i) => (
           <Image
             key={i}
             className="container h-[250px] object-contain"
