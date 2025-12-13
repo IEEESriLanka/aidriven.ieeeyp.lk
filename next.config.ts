c2c42365fb2ee28eafde4a7eaa3b5f09686c6afb
Dinil Thilakarathne <126305123+Dinil-Thilakarathne@users.noreply.github.com>
2025-12-14 03:12:23 +0530

    feat: remove github pages configs

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-// const repoName = "/aidriven.ieeeyp.lk";
 
-
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-  // Ensure static assets resolve correctly when hosted at /<repo-name>/ on GitHub Pages.
-  // basePath: repoName,
-  // assetPrefix: repoName,
-  // // trailingSlash: true,
-  // output: "export",
-  // distDir: "dist",
-};
+const nextConfig = {};
 export default nextConfig;
